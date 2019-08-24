cff5a5a349b6bd09020ac5f917cc510ac5191780
Wander Neto <wander.dmneto@gmail.com>
2019-08-24 19:04:18 +1000

    frontend project created

---
+++ frontend/my-goal/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import { Provider} from 'react-redux'
+
+import reducers from './main/reducers'
+import App from './main/App';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+        && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(reducers, devTools)
+
+ReactDOM.render(
+    <Provider store={store}>
+            <App />
+    </Provider>
+, document.getElementById('app'))
